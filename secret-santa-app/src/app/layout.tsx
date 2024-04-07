e1b621156235708a914396cf7044f64a77e1a148
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-04-07 13:05:21 +0500

    add: 2 -- UI mui
    npm install @mui/material
    
    npm install @mui/material-nextjs @emotion/cache
    
    npm install @emotion/react
    
    npm install @emotion/styled
    
    
    https://mui.com/material-ui/integrations/nextjs/
    https://mui.com/material-ui/customization/theming/
    https://zenoo.github.io/mui-theme-creator/#Buttons
    https://m2.material.io/inline-tools/color/

--- secret-santa-app/src/app/layout.tsx
+++ secret-santa-app/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "../app/globals.css";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+      <AppRouterCacheProvider>
+        {children}
+      </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
