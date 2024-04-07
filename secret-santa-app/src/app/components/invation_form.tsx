01ce35eaf68f5d79516d99571fe6494b7a1be5dc
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-04-08 00:15:52 +0500

    add: nav component to mn header

--- secret-santa-app/src/app/components/invation_form.tsx
+++ secret-santa-app/src/app/components/invation_form.tsx
@@ -1,24 +1,39 @@
 "use client";
 
-import Button, { ButtonProps } from '@mui/material/Button';
+import { Box, Button, Grid, Typography } from '@mui/material';
 import { MainButton } from "@/helpers/uiHelpers";
+import { styled } from '@mui/material/styles';
 
-export default function Invation_form() {
+const MainButton1 = styled(MainButton)({
+  margin: '10px',
+});
+
+const StyledBox = styled(Box)({
+  backgroundColor: '#f0f0f0',
+  padding: '20px',
+  borderRadius: '10px',
+});
 
 
+export default function InvitationForm() {
   return (
-    <div>
-      <form action="#">
-      <div>
-        <MainButton variant="contained">Создать свою карточку участников</MainButton>
-        </div>
-        <div>
-        <MainButton variant="contained">Добавить участников вручную</MainButton>
-        </div>
-        <div>
-        <MainButton variant="contained">Пригласить по ссылки</MainButton>
-        </div>
-      </form>
-    </div>
+    <Grid container justifyContent="center" alignItems="center">
+      <StyledBox>
+        <Grid item xs={12}>
+          <Typography align="center" variant="h5" gutterBottom>
+            Добавить участников
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <MainButton1 variant="contained">Создать свою карточку участников</MainButton1>
+        </Grid>
+        <Grid item xs={12}>
+          <MainButton1 variant="contained">Добавить участников вручную</MainButton1>
+        </Grid>
+        <Grid item xs={12}>
+          <MainButton1 variant="contained">Пригласить по ссылке</MainButton1>
+        </Grid>
+      </StyledBox>
+    </Grid>
   );
 }
