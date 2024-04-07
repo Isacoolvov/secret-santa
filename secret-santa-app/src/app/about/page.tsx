71a7b56cfa95a9994af2279b5cb2fb7c63980226
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-04-07 14:31:35 +0500

    add: ui helpers
    example:
    
                <MainButton variant="contained">Создать игру</MainButton>
                <SecondaryButton variant="contained">Жеребьевка</SecondaryButton>
    
                <BootstrapButton variant="contained" disableRipple>
                    Тест бтн
                </BootstrapButton>
                111
                <Button variant="contained" sx={{
                    backgroundColor: 'yellow'
                }}>Создать игру222</Button>
    
    
                <SwitchMain {...label} defaultChecked />
                <SwitchMain {...label} />
                <SwitchMain {...label} disabled defaultChecked />

--- secret-santa-app/src/app/about/page.tsx
+++ secret-santa-app/src/app/about/page.tsx
@@ -1,71 +1,36 @@
 'use client'
 
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Button, { ButtonProps } from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-import { purple } from '@mui/material/colors';
-import { red } from '@mui/material/colors';
+import { SwitchMain, MainButton, SecondaryButton, BootstrapButton } from "@/helpers/uiHelpers";
 
-const BootstrapButton = styled(Button)({
-  boxShadow: 'none',
-  color: '#333',
-  textTransform: 'none',
-  fontSize: 16,
-  borderRadius: '15px',
-  padding: '6px 12px',
-  border: '1px solid',
-  lineHeight: 1.5,
-  backgroundColor: '#FFF',
-  borderColor: '#FF6300',
-  fontFamily: [
-    '-apple-system',
-    'BlinkMacSystemFont',
-    '"Segoe UI"',
-    'Roboto',
-    '"Helvetica Neue"',
-    'Arial',
-    'sans-serif',
-    '"Apple Color Emoji"',
-    '"Segoe UI Emoji"',
-    '"Segoe UI Symbol"',
-  ].join(','),
-  '&:hover': {
-    backgroundColor: '#0069d9',
-    borderColor: '#0062cc',
-    boxShadow: 'none',
-  },
-  '&:active': {
-    boxShadow: 'none',
-    backgroundColor: '#0062cc',
-    borderColor: '#005cbf',
-  },
-  '&:focus': {
-    boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
-  },
-});
+import Switch from "@mui/material/Switch";
 
-const OrangeColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
-  color: '#FFF',
-  borderRadius: '15px',
-  backgroundColor: '#FF6300',
-  '&:hover': {
-    backgroundColor: red[700],
-  },
-}));
+const label = { inputProps: { "aria-label": "Switch demo" } };
+
 
 export default function CustomizedButtons() {
-  return (
-    <Stack spacing={2} direction="row">
-      <OrangeColorButton variant="contained">Создать игру</OrangeColorButton>
-      <BootstrapButton variant="contained" disableRipple>
-        Жеребьевка
-      </BootstrapButton>
-111
-      <Button variant="contained" sx={{
-        backgroundColor: 'yellow'
-      }}>Создать игру222</Button>
+    return (
+        <Stack spacing={2} direction="row">
+            <MainButton variant="contained">Создать игру</MainButton>
+            <SecondaryButton variant="contained">Жеребьевка</SecondaryButton>
 
-    </Stack>
-  );
- }
+            <BootstrapButton variant="contained" disableRipple>
+                Тест бтн
+            </BootstrapButton>
+            111
+            <Button variant="contained" sx={{
+                backgroundColor: 'yellow'
+            }}>Создать игру222</Button>
+
+
+            <SwitchMain {...label} defaultChecked />
+            <SwitchMain {...label} />
+            <SwitchMain {...label} disabled defaultChecked />
+        </Stack>
+
+
+
+    );
+}
