eb0645a0a1f927a9dc334535518f949c0db7a7d4
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-04-09 21:02:29 +0500

    add: action navigate

--- secret-santa-app/src/app/actions/navigate_to_add_participiant_action.tsx
+++
@@ -1,9 +0,0 @@
-
-'use server';
- 
-import { redirect } from 'next/navigation'
- 
-export async function navigateToAddParticipiant (data: FormData) {
-  redirect(`/invitation_participiant/${data.get('id')}`)
-}
-
