e9d053002fab4b92d00390c369db95f8f04ffc8b
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-04-12 00:31:03 +0500

    add navigate_to_reshuffle

---
+++ secret-santa-app/src/app/actions/navigate_to_reshuffle.tsx
@@ -0,0 +1,40 @@
+export async function navigate_to_reshuffle(prevState: any, participantsData: FormData) {
+    let successMessage = '';
+    let errorMessage = '';
+  
+    try {
+      const response = await fetch(`http://51.107.14.25:8080/games/${participantsData.get('gameId')}/reshuffle`, {
+        method: 'POST',
+        headers: {
+          'accept': '*/*',
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${participantsData.get('access')}`,
+        },
+        body: JSON.stringify([{}]),
+      });
+  
+      console.log('reshuffle post navigateTo...');
+      console.log(participantsData.get('access'));
+  
+      if (!response.ok) {
+        errorMessage = await response.text() || 'Ошибка при отправке данных на сервер';
+        throw new Error(errorMessage);
+      }
+  
+       if (response.headers.get('Content-Type') === 'application/json') {
+        const responseBody = await response.json();
+        if (response.status === 200) {
+          successMessage = 'Жеребьевка завершена';
+        } 
+      } else {
+         const responseData = await response.text();
+         successMessage ='' + responseData;
+      }
+    } catch (error) {
+      errorMessage = error.message;
+      errorMessage = ('Ошибка:' +  errorMessage);
+    }
+  
+    return { successMessage, errorMessage };
+  }
+  
