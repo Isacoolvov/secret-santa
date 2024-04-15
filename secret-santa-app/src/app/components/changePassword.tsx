0fe71250400ea4ea2b05694033ec95c122ef00c9
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-04-15 14:08:58 +0500

    refactor: design input

--- secret-santa-app/src/app/components/changePassword.tsx
+++ secret-santa-app/src/app/components/changePassword.tsx
@@ -3,33 +3,69 @@ import { useFormState } from "react-dom";
 import changeP from "../actions/change-password";
 import '../../css/myAccount.css'
 import DeleteAccount from "./deleteAccount";
+
+import { Box, Grid, TextField, Button, Typography } from '@mui/material';
+import { MainButton1, StyledBox } from "@/helpers/styles";
+import Stack from '@mui/material/Stack';
+
 export default function changePassword() {
-  const [formstate, dispatch] = useFormState(changeP,  null);
+  const [formstate, dispatch] = useFormState(changeP, null);
   console.log(formstate);
-  
+
   return (
     <div>
-      <form action={dispatch}>
-        <h2 className="form-h2">Пароль</h2>
-        <label>
-        Текущий пароль
-          <input type="password" name="currentPassword"/>
-        </label>
-        <label>
-        Новый пароль
-          <input type="password" name="newPassword" />
-        </label>
-        <label>  
-        Повторите пароль
-          <input type="password" name="confirmPassword"/>
-        </label>
-        <br />
-        <button className="save-btn" type="submit">Сохранить</button>
-        <p className="result"> {formstate?.data===''?<>{formstate.error} Текущий пароль не совпадает </>:<>{formstate?.data}</>}</p>
 
-    
+      <p className="result"> {formstate?.data === '' ? <>{formstate.error} Текущий пароль не совпадает </> : <>{formstate?.data}</>}</p>
+
+
+      <form action={dispatch}>
+
+        <Grid item xs={12}>
+          <Typography align="center" variant="h5" gutterBottom>
+            Сменить пароль
+          </Typography>
+        </Grid>
+
+        <Stack m={2} spacing={3}>
+
+          <TextField
+            label="Текущий пароль"
+            variant="outlined"
+            fullWidth
+            name="currentPassword"
+          />
+
+          <TextField
+            label="Новый пароль"
+            variant="outlined"
+            fullWidth
+            name="newPassword"
+          />
+
+          <TextField
+            label="Повторите пароль"
+            variant="outlined"
+            fullWidth
+            name="confirmPassword"
+          />
+
+          <MainButton1
+            variant="contained"
+            type="submit"
+            style={{
+              marginLeft: "-24px",
+              height: "50px",
+              marginTop: "70px",
+            }}
+          >
+            Сохранить
+          </MainButton1>
+
+
+
+        </Stack>
       </form>
-      <DeleteAccount/>
+      <DeleteAccount />
     </div>
   );
 }
