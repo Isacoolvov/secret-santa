c4af50794d530b6b2512b5d11f576d3aaa02b35c
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-04-15 10:30:28 +0500

    refactor: change to TextField  signup.tsx

--- secret-santa-app/src/app/components/signup.tsx
+++ secret-santa-app/src/app/components/signup.tsx
@@ -4,9 +4,13 @@ import signup from "../actions/sign-up";
 import saveTokens from "@/helpers/save-tokens";
 import { MainButton1, StyledBox } from "@/helpers/styles";
 import Link from "next/link";
-import { Grid, Typography } from "@mui/material";
+import { Box, Grid, TextField, Button, Typography } from '@mui/material';
 import "@/css/myAccount.css";
-import { redirect} from "next/navigation";
+import { redirect } from "next/navigation";
+import Stack from '@mui/material/Stack';
+
+
+
 export default function SingUp() {
   const [formstate, dispatch] = useFormState(signup, {
     error: null,
@@ -25,49 +29,63 @@ export default function SingUp() {
   return (
     <>
       <Grid container justifyContent="center" alignItems="center">
-        <StyledBox style={{ width: "550px", height: "600px" }}>
-          <Typography
-            align="center"
-            variant="h5"
-            gutterBottom
-            style={{ marginTop: "50px", marginBottom: "40px" }}
-          >
-            <b> Регистрация</b>
-          </Typography>
-          <form className="form" action={dispatch}>
-            <label>
-              Ваше Login
-              <input type="text" name="name" placeholder="Имя Фамилия" />
-            </label>
-            <br />
-            <label>
-              Ваш E-mail
-              <input
-                type="email"
-                name="email"
-                placeholder="exemple@gmail.com"
-              />
-            </label>
-            <label>
-              Ваш Password
-              <input type="password" name="password" placeholder="password" />
-            </label>
+        <StyledBox>
 
-            <br />
-            <span style={{ marginLeft: "190px", color: "red" }}>
-              {formstate?.error}
-            </span>
-            <MainButton1
-              variant="contained"
-              type="submit"
-              style={{
-                marginLeft: "-24px",
-                height: "50px",
-                marginTop: "70px",
-              }}
-            >
-              Зарегистрироваться
-            </MainButton1>
+
+          <Grid item xs={12}>
+            <Typography align="center" variant="h5" gutterBottom>
+              Регистрация
+            </Typography>
+          </Grid>
+
+          <span style={{ marginLeft: "190px", color: "red" }}>
+            {formstate?.error}
+          </span>
+
+          <form className="form" action={dispatch}>
+
+            <Stack m={2} spacing={3}>
+
+
+
+              <TextField
+                label="Ваше Имя"
+                variant="outlined"
+                fullWidth
+                name="name"
+              />
+
+              <TextField
+                label="Ваш E-mail"
+                variant="outlined"
+                fullWidth
+                name="email"
+              />
+
+              <TextField
+                label="Ваш пароль"
+                variant="outlined"
+                fullWidth
+                name="password"
+              />
+
+
+
+
+              <MainButton1
+                variant="contained"
+                type="submit"
+                style={{
+                  marginLeft: "-24px",
+                  height: "50px",
+                  marginTop: "70px",
+                }}
+              >
+                Зарегистрироваться
+              </MainButton1>
+
+            </Stack>
+
           </form>
           <span style={{ marginLeft: "190px" }}>
             Уже есть аккаунт? <Link href="/login">Войти</Link> {" "}
