f650ad00e8e27bcaccd7d97d1dc03a9e59190166
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-04-15 13:54:41 +0500

    refactor: design input page.tsx

--- secret-santa-app/src/app/myAccount/page.tsx
+++ secret-santa-app/src/app/myAccount/page.tsx
@@ -1,20 +1,33 @@
+"use client";
+
+
 import MyAccount from "../components/myAccount";
 import ChangePassword from "../components/changePassword";
 import DeleteAccount from "../components/deleteAccount";
 import '../../css/myAccount.css'
 import UserInfo from '../components/user-info'
+import { Box, Grid, TextField, Button, Typography } from '@mui/material';
+import { MainButton1, StyledBox } from "@/helpers/styles";
+
 
 export default function Page() {
   return (
-    <main className="page-container"> 
+    <Grid container justifyContent="center" alignItems="center">
+    <StyledBox>
    
-      <div className="forms">
+      
       <UserInfo/>
-        <h2 className="form-h2">Личные данные</h2>
+
+        <Grid item xs={12}>
+            <Typography align="center" variant="h5" gutterBottom>
+            Личные данные
+            </Typography>
+          </Grid>
+
         <MyAccount />
         <ChangePassword />
-      </div>
-    </main>
+        </StyledBox>
+      </Grid>
   );
   
 }
