ea5edee135f0b9f5b5e773af14abd4729081dc88
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-04-17 01:05:41 +0500

    Update contactInfo.tsx

--- secret-santa-app/src/app/components/contactInfo.tsx
+++ secret-santa-app/src/app/components/contactInfo.tsx
@@ -1,19 +1,22 @@
 "use client"
 import { useEffect, useState } from "react";
-import { Typography, Grid } from "@mui/material";
+import { Typography, Grid, TextField } from "@mui/material";
 import { getAccessToken } from "@/helpers/getTokens";
 import { MainButton1, StyledBox } from "@/helpers/styles";
-import {  useParams, useRouter } from "next/navigation";
-import  {navigateToWishList}  from "../navigation/navigate_to_wishList";
-import '@/css/myAccount.css'
+import { useParams, useRouter } from "next/navigation";
+import { navigateToWishList } from "../navigation/navigate_to_wishList";
+import Stack from '@mui/material/Stack';
+
+
+
 export default function UserInfo() {
-  const router =useRouter()
+  const router = useRouter()
   const [isLoading, setLoading] = useState(true);
   const access = getAccessToken();
   const game = localStorage.getItem('id')
   const fetchUrl = `http://51.107.14.25:8080/gameuser/${localStorage.getItem('id')}/contact-info`;
 
-  const handleSubmit = async (event:any) => {
+  const handleSubmit = async (event: any) => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const email = formData.get("email");
@@ -53,30 +56,50 @@ export default function UserInfo() {
           >
             <b>Контактные данные </b>
           </Typography>
-          <form className="form"  onSubmit={handleSubmit}>
-            <label>
-              Ваш E-mail
-              <input
-                type="email"
+
+
+
+          <form className="form" onSubmit={handleSubmit}>
+            <Stack m={2} spacing={3}>
+
+              <TextField
+                label="Ваш E-mail"
+                variant="outlined"
+                fullWidth
                 name="email"
-               
               />
-            </label>
-            <label>
-            Ваше Имя
-              <input type="text" name="userName"  />
-            </label>
-            <label>
-            Номер телефона
-              <input type="text" name="userPhoneNumber"  />
-            </label>
-            <MainButton1 
-            variant="contained"
-           type="submit"
-            style={{marginTop:'75px',marginLeft: '190px' ,width:'180px' ,height:'50px' }}
-          >
-        Далее
-          </MainButton1>
+
+
+
+              <TextField
+                label="Ваше Имя"
+                variant="outlined"
+                fullWidth
+                name="userName"
+              />
+
+
+              <TextField
+                label="Номер телефона"
+                variant="outlined"
+                fullWidth
+                name="userPhoneNumber"
+              />
+
+              <MainButton1
+                variant="contained"
+                type="submit"
+                style={{
+
+                  height: "50px",
+                  marginBottom: "70px",
+                  marginTop: "30px",
+                }}              >
+                Далее
+              </MainButton1>
+
+            </Stack>
+
           </form>
         </StyledBox>
       </Grid>
