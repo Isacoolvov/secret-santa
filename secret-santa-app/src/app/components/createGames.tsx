fb21789f437fe2764a192ce49770007c9676925b
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-04-17 00:59:20 +0500

    Update createGames.tsx

--- secret-santa-app/src/app/components/createGames.tsx
+++ secret-santa-app/src/app/components/createGames.tsx
@@ -5,17 +5,18 @@ import '../../css/myAccount.css'
 import { useState } from "react";
 import localsaveGameId from "@/helpers/saveGameId";
 import { useRouter } from "next/navigation";
-import { Box, Grid, Typography, styled } from "@mui/material";
+import { Box, Grid, Typography, styled, TextField } from "@mui/material";
 import { MainButton, SwitchMain } from "@/helpers/uiHelpers";
 import { MainButton1, StyledBox } from "@/helpers/styles";
+import Stack from '@mui/material/Stack';
 
 export default function CreateGames() {
   const [formstate, createdispatch] = useFormState(createGame, {
     error: null,
     data: null,
   });
-  const router =useRouter()
-  const [priceLimitChecked ,setPriceLimitChecked] = useState(false)
+  const router = useRouter()
+  const [priceLimitChecked, setPriceLimitChecked] = useState(false)
   console.log(formstate);
   if (formstate?.data != null) {
     localsaveGameId(formstate.data.id);
@@ -23,55 +24,83 @@ export default function CreateGames() {
     router.push(`/gameCreated/${formstate.data.id}`)
   }
   return (
-    <div>
-        
-
-    <Grid container justifyContent="center" alignItems="center">
+   
 
 
- <StyledBox style={{width: '550px' , height:'600px'}}> 
- <Typography align="center" variant="h5" gutterBottom style={{marginTop:'50px' ,marginBottom:'40px'}} >
-<b> Создать игру</b>
+      <Grid container justifyContent="center" alignItems="center">
+
+
+        <StyledBox>
+          <Typography align="center" variant="h5" gutterBottom style={{ marginTop: '50px', marginBottom: '40px' }} >
+            <b> Создать игру</b>
           </Typography>
-      <form className="form" action={createdispatch}>
+          <form className="form" action={createdispatch}>
 
-        <label >
-          Название игры
-        <input  type="text" name="name" placeholder="Название игры"/>
-        </label>
-        <br />
-        <label >
-        Идентификатор
-        <input  type="text" name="name" placeholder="random"/>
-        </label>
-        <label >
-        <span>Максимальная стоимость подарка</span>
-         <SwitchMain 
-        defaultChecked={priceLimitChecked} 
-        onClick={(e: React.MouseEvent<HTMLButtonElement>) => setPriceLimitChecked(!priceLimitChecked)} />
-      
-              </label>  
-              <p style={{fontSize:'0.63rem' , marginLeft:'40px' , }}>При включенной опции участникам будет показано ограничение, которому они должны будут следовать</p>
-              <br />
-       {priceLimitChecked===true?<>
-        <input type="number" name="maxPrice"   placeholder="Укажите максимальную стоимость подарка" />
-         </>:<></>}
-        <br/>  
-        <MainButton1 
-            variant="contained"
-           type="submit"
-            style={{marginLeft: '190px' ,width:'180px' ,height:'50px' }}
-          >
-        Создать игру
+            <Stack m={2} spacing={3}>
 
-          </MainButton1>
-              </form>
-              </StyledBox>
+
+              <TextField
+                label="Название игры"
+                variant="outlined"
+                fullWidth
+                name="name"
+              />
 
 
 
-</Grid>
+              <TextField
+                label="Идентификатор (Random)"
+                variant="outlined"
+                fullWidth
+                name="random"
+              />
 
-    </div>
+
+
+
+              <label >
+                <Typography variant="body1">
+                  <span>Максимальная стоимость подарка</span>
+                </Typography>
+                <SwitchMain
+                  defaultChecked={priceLimitChecked}
+                  onClick={(e: React.MouseEvent<HTMLButtonElement>) => setPriceLimitChecked(!priceLimitChecked)} />
+
+              </label>
+              <Typography variant="body2" style={{ fontSize: '1rem', marginLeft: '40px', }}>При включенной опции участникам будет показано ограничение, которому они должны будут следовать </Typography>
+              {priceLimitChecked === true ? <>
+
+
+                <TextField
+                  label="Укажите максимальную стоимость подарка"
+                  variant="outlined"
+                  fullWidth
+                  name="maxPrice"
+                />
+
+
+
+              </> : <></>}
+              
+              <MainButton1 
+                variant="contained"
+                type="submit"
+                style={{ width: '11.25rem', height: '3.125rem' }}
+              >
+                Создать игру
+
+              </MainButton1>
+
+            </Stack>
+
+
+          </form>
+        </StyledBox>
+
+
+
+      </Grid>
+
+   
   );
 }
